0551800d1d313d8853d954d0103d786819654978
juancastaneda1 <juanfelips97@hotmail.com>
2020-09-12 15:50:44 -0400

    Completed web scraper
    Web scraper now grabs the latest data and updates the csv file accordingly.

--- get_covid_data.py
+++ get_covid_data.py
@@ -1,51 +1,72 @@
-
-url = 'https://www.ontario.ca/page/how-ontario-is-responding-covid-19'
-
-# pip install selenium
-# pip install beautifulSoup4
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
-
 import time
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from datetime import timedelta
+
+
+url = 'https://www.ontario.ca/page/how-ontario-is-responding-covid-19'
 
 #stop firefox from actually opening 
 options = Options()
 options.headless = True
 
 # download geckodriver.exe and change path
-driver = webdriver.Firefox(firefox_options=options, executable_path=r'C:\\Users\\Mohamed\\Anaconda2\\envs\\pytorch\\Lib\\site-packages\\selenium\\geckodriver.exe')
-
-
+driver = webdriver.Firefox(options=options, executable_path=r'C:\Program Files\Geckodriver\geckodriver.exe')
 driver.get(url)
 
 
-# just incase wait a bit (NOT SURE IF THIS IS NEEDED)
+# wait for page to load all features
 time.sleep(30)
 
+
+# get page html
 html = driver.page_source
+soup = BeautifulSoup(html, features='lxml')
 
-soup = BeautifulSoup(html,features='lxml')
 
-
+# get table data
 all_data = []
 data = []
 
 for table in soup.find_all('table', attrs={'class':'numeric full-width'}):
-    
-    table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
+    rows = table.tbody.find_all('tr')
     
     for row in rows:
-        cols = row.find_all('td')
-        cols = [val.text.strip() for val in cols]
-        data.append([val for val in cols if val])
+        elements = row.find_all('td')
+        elements = [val.text.strip().split('\n')[0] for val in elements]
+        data.append([val for val in elements if val])
         
     all_data.append(data)
     data = []
-    
-print(all_data)
-    
-    
 
+
+# get yesterday's date (data is from previous day)
+date = datetime.strftime(datetime.now() - timedelta(1), "%m/%d/%Y")
+
+
+# get data as a pandas dataframe
+df1 = pd.DataFrame(np.array(all_data[0])[[0,1,2,6],0])
+df2 = pd.DataFrame(all_data[1])
+df3 = pd.DataFrame(np.array(all_data[2])[[0,2]])
+df4 = pd.DataFrame(all_data[3])
+
+# remove commas from numbers
+df1[0] = df1[0].str.replace(',', '')
+df2[0] = df2[0].str.replace(',', '')
+df3[0] = df3[0].str.replace(',', '')
+df4[0] = df4[0].str.replace(',', '')
+
+df = pd.concat([df1, df2, df3, df4])
+
+# get data from previous days from csv
+data = np.genfromtxt(r'Ontario Covid19 data.csv', delimiter=',', encoding="utf-8-sig", dtype=None)
+data = pd.DataFrame(data[1:, :], columns=data[0])
+
+#add new data and save to csv
+data.insert(1,date, df.to_numpy().astype(int), allow_duplicates=True)
+data.to_csv(r'Ontario Covid19 data.csv', index=False)
 
